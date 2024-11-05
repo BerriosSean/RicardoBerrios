0c56167fcef978fae8cc41ff6a0f01616a454933
BerriosSean <ricardo.berrios.m@uni.pe>
2024-11-05 15:29:11 -0500

    Create Ejercicio_1.cpp

---
+++ Ejercicio_1.cpp
@@ -0,0 +1,15 @@
+/*
+Solicita al usuario un entero y crea un arreglo dinámico de ese tamaño.
+Luego, llena el arreglo con números ingresados por el usuario.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+  return 0;
+}
