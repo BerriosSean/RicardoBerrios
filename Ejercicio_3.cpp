8f95296c175a638b26791b99d46b6aa082381091
BerriosSean <ricardo.berrios.m@uni.pe>
2024-11-05 15:51:32 -0500

    Create Ejercicio_3.cpp

---
+++ Ejercicio_3.cpp
@@ -0,0 +1,12 @@
+/*
+Escribe un programa que pida al usuario una cadena de caracteres y la
+almacene dinámicamente en memoria.
+*/
+#include <iostream>
+using namespace std;
+
+int main (){
+
+
+return 0;
+}
